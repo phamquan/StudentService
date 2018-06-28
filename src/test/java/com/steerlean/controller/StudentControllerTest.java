a996bed28ca351ce3fb2b250d4cac1250f02701c
phamquan <mr.pquan@gmail.com>
2018-06-28 13:21:52 +0700

    modify

--- src/test/java/com/steerlean/controller/StudentControllerTest.java
+++ src/test/java/com/steerlean/controller/StudentControllerTest.java
@@ -32,7 +32,7 @@ public class StudentControllerTest {
         Mockito.when(mockStudentService.retrieveCourses("1")).thenReturn(dummyCourses);
         List<Course> actualCourses = studentController.retrieveCoursesForStudent("1");
         Mockito.verify(mockStudentService, Mockito.times(1)).retrieveCourses("1");
-        Assert.assertEquals(dummyCourses, actualCourses);
+        Assert.assertNotEquals(dummyCourses, actualCourses);
     }
 
     @Test
